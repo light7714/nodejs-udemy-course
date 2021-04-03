de6ad68125a43865da0b52bc634e911982c400cd
Shubham Vermani <svermani_be19@thapar.edu>
2021-04-03 12:28:13 +0530

    Added 6th module

---
+++ myCode/6-templatingEngines/app.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const rootDir = require("./util/path");
+
+const app = express();
+
+PORT = 8000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+app.listen(8000, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+// console.log(__dirname);
